69a84f2843dd5f05c0002923a63e3f281eacd625
Vishnusidmec <vishnu@sidmectech.in>
2024-08-30 18:07:05 +0530

    first commit

---
+++ school_management/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "School Managemet",
+    'summary': "Helps to manage the school details",
+    'version': '2.0',
+    'depends': [],
+    'auto_install': [],
+    'data': ["security/ir.model.access.csv",
+        "views/student_view.xml",
+"views/teacher_view.xml",
+"views/school_menu.xml",
+
+
+    ],
+
+    'license': 'LGPL-3',
+}
