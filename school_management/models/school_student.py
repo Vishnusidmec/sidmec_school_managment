1ddb7ab8f9c3fcaf1962cf0a92cb27b890451bcb
Vishnusidmec <vishnu@sidmectech.in>
2024-08-31 14:18:52 +0530

    first commits

--- school_management/models/school_student.py
+++ school_management/models/school_student.py
@@ -2,6 +2,7 @@ from email.policy import default
 
 from odoo import fields,models,api
 from odoo.tools.populate import compute
+from datetime import datetime
 
 
 class SchoolStudent(models.Model):
@@ -11,22 +12,39 @@ class SchoolStudent(models.Model):
     guardian_name = fields.Char(string="Guardian name", required=True)
     guardian_phone = fields.Char(string="Guardian_phone")
     date_of_birth = fields.Date(string="Date of birth" )
-    age = fields.Char(string="Age")
+    age = fields.Char(string="Age",compute='_compute_age',store=True)
     address = fields.Char(string="Address")
     standard= fields.Char(string="Class")
+    teacher_mob = fields.Char(string="Teacher Mobile")
     class_teacher=fields.Many2one('school.teacher', string="class_teacher")
+
     fee_structure_ids = fields.One2many('fees.structure', 'student_id', string='Fees Structures',
                                         help="Related Fees Structure")
     teacher_ids = fields.Many2many(
         'school.teacher', string="Other Subject Teachers",
         help='Mention the teachers who teach other subjects.')
-    select_status = fields.Selection([('not_selected', 'Not Selected'),('selected' , 'Selected')],default='not_selected')
-    age_date_of_birth = fields.Boolean(compute='age_finding',store=True)
+
+    select_status = fields.Selection([('draft', 'Draft'), ('admitted', 'Admitted')],
+                                        default='draft')
 
     def action_select(self):
-        self.select_status = 'selected'
+        self.select_status = 'admitted'
 
 
-    @api.depends('date_of_birth','age_date_of_birth')
-    def age_finding(self):
-        print("working fine")
+
+
+#function for teacher mobile automatically update when student select the teacher
+    @api.onchange('class_teacher')
+    def _onchange_class_teacher(self):
+        if self.class_teacher:
+            self.teacher_mob = self.class_teacher.mobile
+
+#function for calculating age automatically when DOB entered
+    @api.depends('date_of_birth')
+    def _compute_age(self):
+        for record in self:
+            if record.date_of_birth:
+                today = datetime.today()
+                dob = fields.Date.from_string(record.date_of_birth)
+                age = today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day))
+                record.age = age
